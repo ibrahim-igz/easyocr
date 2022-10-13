82e6180bab76d4cb060d585864ee8e347fd59fd4
saleh-igz <igz.iwd.31@gmail.com>
2022-10-13 10:26:36 +0500

    first deploy

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask
+import flask
+import json
+import easyocr
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+count = 0
+if count == 0:
+    reader = easyocr.Reader(['en'], gpu=False)
+    count += 1
+
+
+def applying_ocr(img):
+    return reader.readtext(img, detail=0)
+
+def processing(image):
+    text = applying_ocr(image)
+    credential = []
+    for i in range(len(text)):
+        if len(text[i]) == 0:
+            pass
+        elif text[i][0].lower() == 'n' and text[i][-1].lower() == 'e' and len(text[i]) <= 5:
+            credential.append(text[i + 1])
+        elif 'ame' in text[i].lower() and len(text[i]) <= 5:
+            credential.append(text[i + 1])
+        elif 'nam' in text[i].lower() and len(text[i]) <= 5:
+            credential.append(text[i + 1])
+        elif '-' in text[i] and len(text[i]) >= 13:
+            credential.append(text[i])
+            break
+    if len(credential) == 2 or len(credential) == 3:
+        data = {'Name': credential[0], 'ID Num': credential[-1]}
+    else:
+        data = 'None'
+
+    return data
+
+
+def main_func(encoded):
+    numpy = np.array(encoded, dtype='uint8')
+    decode = cv2.imdecode(numpy, cv2.IMREAD_COLOR)
+    data = processing(decode)
+    return data
+
+
+
+
+@app.route('/', methods=['POST'])
+def test():
+    input_1 = json.loads(flask.request.data.decode('utf-8'))['img_to_ocr']
+
+    return main_func(input_1)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
